282dc382c7540935a6c5ef9eb4eb38ee87366ef5
Gagan Ichake <gaganichake@yahoo.com>
2022-03-31 17:00:30 -0500

    Clean up imports

--- src/main/java/com/gagan/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/gagan/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -3,7 +3,6 @@ package com.gagan.rest.webservices.restfulwebservices.filtering;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonFilter("SomeBeanFilter")
 public class SomeBean {
--- src/main/java/com/gagan/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/gagan/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,6 @@
 package com.gagan.rest.webservices.restfulwebservices.user;
 
-import org.hibernate.EntityMode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
