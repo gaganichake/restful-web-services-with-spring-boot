39f84ae9a41ce2df96c05fc7a435014bc098232b
Gagan Ichake <gaganichake@yahoo.com>
2022-03-31 11:58:52 -0500

    Versioning - Basic Approach with URIs

---
+++ src/main/java/com/gagan/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,13 @@
+package com.gagan.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/gagan/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,13 @@
+package com.gagan.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+}
