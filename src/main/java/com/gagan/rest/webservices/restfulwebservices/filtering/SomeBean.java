63c59664f3622fd8ef73e35d1e10efe468850af4
Gagan Ichake <gaganichake@yahoo.com>
2022-03-31 11:47:08 -0500

    Dynamic Filtering

--- src/main/java/com/gagan/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/gagan/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,10 +1,11 @@
 package com.gagan.rest.webservices.restfulwebservices.filtering;
 
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-//@JsonIgnoreProperties(value={"field1", "field2"}) // Alternative
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
     private String field1;
