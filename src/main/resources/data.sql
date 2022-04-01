d4361fb399691a63ad5bdf59ab4161ee470d418d
Gagan Ichake <gaganichake@yahoo.com>
2022-04-01 09:12:45 -0500

    Post Entity and Many to One Relationship with User Entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into USER values(1, sysdate(), 'Tim');
 insert into USER values(2, sysdate(), 'June');
 insert into USER values(3, sysdate(), 'Joe');
+insert into POST values(11, 'My First Post', 1);
+insert into POST values(12, 'My Second Post', 1);
